cff78ca3bad1e5984c0863ca13ead2a340bf07c2
mohd.zaki <mohd.zaki@impact-multimedia.com>
2025-04-17 01:25:02 +0800

    feat: added assets from previous tutorial project for personal reference and reuse

---
+++ app/Http/Controllers/NinjaController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use App\Models\Dojo;
+use App\Models\Ninja;
+use Illuminate\Http\Request;
+
+class NinjaController extends Controller
+{
+    public function index(){
+        $ninjas = Ninja::with('dojo')->orderBy('created_at', 'desc')->paginate(10);
+        
+        return view('ninjas.index', ["ninjas" => $ninjas]);
+    }
+
+    public function show($id){
+        $ninja = Ninja::with('dojo')->findOrFail($id);
+        return view('ninjas.show', ["ninja" => $ninja]);
+    }
+
+    public function create(){
+        $dojos = Dojo::all();
+
+        return view('ninjas.create', ["dojos" => $dojos]);
+    }
+
+    public function store(Request $request){
+        $validated = $request->validate([
+            'name' => 'required | string | max:255',
+            'skill' => 'required | integer | min:0 | max: 100',
+            'bio' => 'required | string | min:20 | max:1000',
+            'dojo_id' => 'required | exists:dojos,id' //check id exist in dojo table
+        ]);
+
+        Ninja::create($validated);
+
+        return redirect()->route('ninjas.index');
+    }
+
+
+
+
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\NinjaController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('ninjas/', [NinjaController::class, 'index'])->name('ninjas.index');
+Route::get('ninjas/create', [NinjaController::class, 'create'])->name('ninjas.create');
+Route::get('ninjas/{id}', [NinjaController::class, 'show'])->name('ninjas.show');
+Route::post('/ninjas', [NinjaController::class, 'store'])->name('ninjas.store');
