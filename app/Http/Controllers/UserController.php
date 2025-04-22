a1dd797b92ce76655fbb3d5f0f95764b91173386
mohd.zaki <mohd.zaki@impact-multimedia.com>
2025-04-22 20:05:20 +0800

    values on dashboard, reflect database values. add links to project, outstanding invoice dashboard next

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,9 +3,15 @@
 namespace App\Http\Controllers;
 
 use App\Models\Client;
+use App\Models\Income;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Database\Eloquent\Builder;
+use App\Models\Invoice;
+use App\Models\Project;
 
 class UserController extends Controller
 {
@@ -14,13 +20,36 @@ class UserController extends Controller
     {
         $clients = Client::where('freelancer_id', Auth::user()->id)->orderBy('created_at', 'desc')->paginate(10);
 
+        //Monthly Income
+        $currentYearMonth = Carbon::now()->format('Y-m');
+        $freelancer = Auth::user();
 
-        return view('freelancer.dashboardClients', ["clients" => $clients]);
+        $targetMonthIncome = Income::where('freelancer_id', $freelancer->id)->where('year_month', $currentYearMonth)->first();
+
+        $monthlyIncome = 0;
+
+        if ($targetMonthIncome) {
+            $monthlyIncome = $targetMonthIncome->income;
+        }
+
+        //Number of Clients
+        $numberOfClients = $freelancer->client->count();
+        Log::error('client count is: ' . $numberOfClients);
+
+        // Number of projects
+        $projectCount = Project::whereHas('client', function($query) use ($freelancer){
+            $query->where('freelancer_id', $freelancer->id);
+        })->count();
+
+        //Outstanding Invoices
+        //'project.client' means go up the relationship chain from Invoice -> Project -> Client
+        $outstandingInvoiceCount = Invoice::where('isPaid', false)->whereHas('project.client', function ($query) use ($freelancer) {
+            $query->where('freelancer_id', $freelancer->id);
+        })->count();
+        Log::error('unpaid invoice count is: ' . $outstandingInvoiceCount);
+
+
+
+        return view('freelancer.dashboardClients', ["clients" => $clients], compact('monthlyIncome', 'numberOfClients', 'projectCount', 'outstandingInvoiceCount'));
     }
-
-    public function exitForm(){
-        return redirect()->back();
-    }
-
-   
 }
--- resources/views/freelancer/dashboardClients.blade.php
+++ resources/views/freelancer/dashboardClients.blade.php
@@ -2,7 +2,7 @@
 
     <div class="flex flex-row items-center gap-x-4 bg-white p-6 rounded shadow max-w-4xl">
         <h2 class="text-lg font-extrabold text-gray-700">Monthly Income :</h2>
-        <p class="text-6xl font-semibold text-green-500">$200</p>
+        <p class="text-6xl font-semibold text-green-500">$ {{$monthlyIncome}}</p>
     </div>
     <div class="container mx-auto px-4 py-8">
 
@@ -10,18 +10,18 @@
 
             <div class="flex-1 bg-white p-6 rounded shadow border-b-4 border-lime-500 group hover:bg-green-300">
                 <h2 class="text-lg text-center font-semibold text-gray-700 group-hover:text-white">Clients</h2>
-                <p class="text-2xl text-center text-blue-500 mt-2 group-hover:text-white">50</p>
+                <p class="text-2xl text-center text-blue-500 mt-2 group-hover:text-white">{{ $numberOfClients }}</p>
             </div>
 
             <div class="flex-1 bg-white p-6 rounded shadow border-b-4 border-lime-500 group hover:bg-green-300">
                 <h2 class="text-lg text-center font-semibold text-gray-700 group-hover:text-white">Projects</h2>
-                <p class="text-2xl text-center text-purple-500 mt-2 group-hover:text-white">30</p>
+                <p class="text-2xl text-center text-purple-500 mt-2 group-hover:text-white">{{ $projectCount }}</p>
             </div>
 
             <div class="flex-1 bg-white p-6 rounded shadow border-b-4 border-lime-500 group hover:bg-green-300">
                 <h2 class="text-lg text-center font-semibold text-gray-700 group-hover:text-white">Outstanding Invoices
                 </h2>
-                <p class="text-2xl text-center text-red-500 mt-2 group-hover:text-white">12</p>
+                <p class="text-2xl text-center text-red-500 mt-2 group-hover:text-white">{{$outstandingInvoiceCount}}</p>
             </div>
         </div>
     </div>
